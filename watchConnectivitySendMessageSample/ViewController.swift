f557a65776af1c2d070e78b0430765abb03e9c74
sohichiro <urochiiuso_ogana_@i.softbank.jp>
2015-09-01 13:09:16 +0900

    Initial Commit

---
+++ watchConnectivitySendMessageSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchConnectivitySendMessageSample
+//
+//  Created by 長尾 聡一郎 on 2015/09/01.
+//  Copyright © 2015年 長尾 聡一郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
